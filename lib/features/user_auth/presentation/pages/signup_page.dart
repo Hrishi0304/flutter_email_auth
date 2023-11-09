34d56a029af2f2bd4aeed7bbadf51b74e558be65
Hrishikesh Addagatla <Hrishi0304>
2023-11-10 01:24:10 +0530

    Sign Up

--- lib/features/user_auth/presentation/pages/signup_page.dart
+++ lib/features/user_auth/presentation/pages/signup_page.dart
@@ -1,17 +1,16 @@
-import 'package:email_auth/features/user_auth/presentation/pages/signup_page.dart';
 import 'package:email_auth/features/user_auth/presentation/widgets/form_container_widgets.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import 'login_page.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: Text("Sign Up"),
       ),
       body: Center(
         child: Padding(
@@ -19,9 +18,16 @@ class LoginPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Login", style : TextStyle(fontSize: 27, fontWeight: FontWeight.bold)),
+              Text("Sign Up", style : TextStyle(fontSize: 27, fontWeight: FontWeight.bold)),
               SizedBox(
                 height: 30,
+              ),
+              FormContainerWidget(
+                hintText: "Username",
+                isPasswordField: false,
+              ),
+              SizedBox(
+                height: 10,
               ),
               FormContainerWidget(
                 hintText: "Email",
@@ -37,31 +43,26 @@ class LoginPage extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder:   (context) => HomePage()));
-                },
-                child: Container(
+              Container(
                   width: double.infinity,
                   height: 45,
                   decoration: BoxDecoration(
                     color: Colors.blue,
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Center(child: Text("Login",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),)
-                ),
+                  child: Center(child: Text("Sign Up",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),)
               ),
               SizedBox(height: 20,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Don't have an account?"),
+                  Text("Already have an account?"),
                   SizedBox(width: 5,),
                   GestureDetector(
                     onTap: (){
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => SignUpPage()), (route) => false);
+                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
                     },
-                    child: Text("Sign Up",style: TextStyle(color: Colors.blue , fontWeight: FontWeight.bold )),
+                    child: Text("Login",style: TextStyle(color: Colors.blue , fontWeight: FontWeight.bold )),
                   )
                 ],
               )
